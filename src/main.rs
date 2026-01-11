c508cafba3d2cdc0b89e0c711e88458417282ae2
lrobin42 <github.com.wafer.759@passmail.net>
2026-01-10 17:36:15 -0800

    second

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,23 @@ use yahoo_finance_api as yahoo;
 fn main() {
     let quote = get_price_history("NVDA".to_string());
 
+    let mut opening_prices = Vec::new();
+    let mut closing_prices = Vec::new();
+    let mut high_prices = Vec::new();
+    let mut low_prices = Vec::new();
+
+    //segment the price data
     for entry in &quote {
-        println!("{:?}", entry.open)
+        opening_prices.push(entry.open);
+        closing_prices.push(entry.close);
+        high_prices.push(entry.high);
+        low_prices.push(entry.low);
     }
-    //println!("{:?}", quote)
+    //calculate 20-day simple moving averages of closing prices.
+    let mut moving_averages: Vec<f64> = calculate_simple_moving_average(closing_prices, 20);
 }
 //Create a function to get the latest price on the security
-fn get_latest_price(ticker: String) -> yahoo_finance_api::Quote {
+fn _get_latest_price(_ticker: String) -> yahoo_finance_api::Quote {
     let provider = yahoo::YahooConnector::new().unwrap();
     // get the latest quotes in 1 minute intervals
     let response = tokio_test::block_on(provider.get_latest_quotes("NVDA", "1d")).unwrap();
@@ -29,6 +39,24 @@ fn get_price_history(ticker: String) -> Vec<yahoo_finance_api::Quote> {
     let response = tokio_test::block_on(provider.get_quote_range(&ticker, "1d", "2y")).unwrap();
     let quotes = response.quotes().unwrap();
     quotes
+}
+
+fn calculate_simple_moving_average(array: Vec<f64>, window: i32) -> Vec<f64> {
+    let interval = window as usize;
+    let mut index = interval - 1;
+    let length = array.len() + 1;
+    let mut results = Vec::new();
+
+    while index < length {
+        index += 1;
+
+        let start_index = index - interval;
+        let interval_slice = &array[start_index..index - 1];
+        let sum: f64 = interval_slice.iter().sum();
+        let interval_float = interval as f64;
+        results.push(sum / interval_float);
+    }
+    results
 }
 
 //Create a function to calculate bollinger bands
